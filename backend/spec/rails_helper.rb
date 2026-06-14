f93e841692a8febdb2b21870a4fe8ef6c8344402
Adansuku <adangrx@gmail.com>
2026-06-14 08:51:27 +0100

    chore: test infra config and schema for active storage
    Regenerates schema.rb for the materials/active_storage migrations,
    enables the Active Job test adapter, adds the "AI" acronym inflection,
    and updates rails_helper test setup.

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -41,6 +42,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
